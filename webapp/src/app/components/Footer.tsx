66fc57fa05fc8ea825c8231ab3e5999fd4ff79c8
Etom <98264570+EtomCoda@users.noreply.github.com>
2025-04-27 07:24:31 -0700

    Delete webapp directory

--- webapp/src/app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="w-full py-4 bg-[#278245] text-center text-white mt-16">
-        © 2024 Bobchi Gas. All rights reserved.
-      </footer>
-    )
-}
